e30b085f962f5e4393bfb242fda7e2edcc14f133
Jovit <jovitsuarez07@gmail.com>
2022-11-25 22:46:43 +0800

    project cards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/reset.css';
 import App from './pages';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,5 +1,12 @@
 import projectCollection  from '../../data/projects';
 import './index.scss'
+
+
+// ant design imports 
+import { Card } from 'antd';
+
+const { Meta } = Card;
+
 function index() {
   return (
     <>
@@ -7,15 +14,21 @@ function index() {
             <h1 className="text-center">Projects</h1>
 
             {projectCollection.map((project, key) => (
-                <div className="" key={key}>
-                    <span>{project.projectName}</span>
-                    <div className="tech">
-                        {project.tech.map((tech, key) => (
-                            <span className='badge bg-dark text-warning' key={key}>{tech}</span>
-                        ))}
-                    </div>
-                    <img src={project.image} className="w-50 rounded"/>
-                    <a href={project.link}>Visit</a>
+                <div className="w-100" key={key}>
+                    <Card
+                        hoverable
+                        size='large'
+                        style={{ width: 500}}
+                        cover={<img alt="project image" src={project.image} />}
+                    >
+                        <div className="tech d-flex justify-content-around">
+                            {project.tech.map((tech, key) => (
+                                <span className='badge bg-dark text-warning p-2' key={key}>{tech}</span>
+                            ))}
+                        </div>
+                        <Meta title={project.projectName}/>
+                        <a href={project.link}>Visit</a>
+                    </Card>
                 </div>
             ))}
         </div>
