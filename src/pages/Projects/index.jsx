72f42b0b7e95cb1a01f13ee03ba9c1c72511e7f4
Jovit <jovitsuarez07@gmail.com>
2022-11-27 20:40:03 +0800

    project layout

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -3,35 +3,44 @@ import './index.scss'
 
 
 // ant design imports 
-import { Card } from 'antd';
+import {GithubOutlined} from '@ant-design/icons'
+import { Button, Card } from 'antd';
 
 const { Meta } = Card;
 
 function index() {
   return (
     <>
-        <div className="projectsWrapper text-light p-2">
+        <section className="projectsWrapper text-light p-2">
             <h1 className="text-center">Projects</h1>
 
             {projectCollection.map((project, key) => (
-                <div className="w-100" key={key}>
+                <div className="row" key={key}>
                     <Card
                         hoverable
                         size='large'
-                        style={{ width: 500}}
-                        cover={<img alt="project image" src={project.image} />}
+                        bodyStyle={{
+                            padding: '10px'
+                        }}
+                        className='w-25 text-center col-md'
+                        cover={<img alt="project image" src={project.image}/>}
                     >
-                        <div className="tech d-flex justify-content-around">
+                        <div className="tech d-flex gap-2 justify-content-center ">
                             {project.tech.map((tech, key) => (
                                 <span className='badge bg-dark text-warning p-2' key={key}>{tech}</span>
                             ))}
                         </div>
-                        <Meta title={project.projectName}/>
-                        <a href={project.link}>Visit</a>
+                        <span className='text-center fw-bold fs-5'>{project.projectName}</span>
+                        <div className='d-flex gap-2 mt-3 justify-content-center'>
+                            <a href={project.code}><GithubOutlined className='text-dark fs-3'/></a>
+                            <a href={project.link}>
+                                <Button className='bg-dark text-light fw-bold border-0'>Visit</Button>
+                            </a>
+                        </div>
                     </Card>
                 </div>
             ))}
-        </div>
+        </section>
     </>
   )
 }
