9b8bffbcb59bc872ac0602b93ce657ff52e6458a
Jovit <jovitsuarez07@gmail.com>
2022-12-01 20:53:45 +0800

    changed main background added footer

--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -1,6 +1,6 @@
 function index() {
   return (
-    <div className="p-2 text-center">Jovit S. Castillo &#169; 2022</div>
+    <div className="p-2 text-center text-light">Jovit S. Castillo &#169; 2022</div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Footer from './Footer'
 function App() {
 	
 	return (
-		<div>
+		<div className='mainBG'>
 			<Home/>
 			<AboutMe/>
 			<Skills/>
