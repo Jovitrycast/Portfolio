b1784ac6d65fd6c10f6f2984aba330319fc89d88
Jovit <jovitsuarez07@gmail.com>
2022-12-05 15:51:54 +0800

    fixed arrow front page icon

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,12 +8,12 @@ const index = () => {
 
     return (
         <>
-            <div className="homeWrapper d-flex align-items-center text-light">
+            <div className="homeWrapper d-flex flex-column align-items-center justify-content-evenly text-light">
                 <Container className='text-center'>
                     <h1 className='fw-bold'>Hello There!</h1>
                     <p className='lead fs-3'>I am <span className='text-warning fw-bold'>Jovit Castillo</span>. I develop websites</p>
                 </Container>
-                <BsChevronDoubleDown className='down-arrow fs-1'/>
+                <BsChevronDoubleDown className='down-arrow fs-1 mx-auto'/>
             </div>
         </>
     );
